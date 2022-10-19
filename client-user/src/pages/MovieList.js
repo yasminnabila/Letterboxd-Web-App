8fe7cb2f23d10b2bf0c9f2c109e7c052d3dc16aa
yasminnabila <yasmnnabila@gmail.com>
2022-10-19 07:41:13 +0700

    Add fetchMovie function

--- client-user/src/pages/MovieList.js
+++ client-user/src/pages/MovieList.js
@@ -10,11 +10,39 @@ function MovieList() {
 
   //? EFFECT ONLY WHEN RENDERED
   useEffect(() => {
-    fetch("http://localhost:4000/movies")
-      .then((response) => response.json())
-      .then((data) => setMovie(data))
-      .catch((err) => console.log(err));
+    fetchMovie();
   }, []);
+
+  async function fetchMovie() {
+    setLoading(true);
+    try {
+      const response = await fetch("http://localhost:4000/movies");
+      if (!response.ok) {
+        throw new Error("Oops! There's something wrong...");
+      }
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  if (loading) {
+    return (
+      <h1 className="text-center justify-content-center align-items-center">
+        {" "}
+        Please wait, page is loading...{" "}
+      </h1>
+    );
+  } else if (error) {
+    return (
+      <h1 className="text-center justify-content-center align-items-center">
+        {" "}
+        Sorry, page cannot be loaded{" "}
+      </h1>
+    );
+  }
 
   return (
     <Container>
