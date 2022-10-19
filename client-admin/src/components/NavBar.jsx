052ac5e53eac9247545a9e4566c1a1fef15181e9
yasminnabila <yasmnnabila@gmail.com>
2022-10-20 03:35:17 +0700

    Feat: SideBar, NavBar

---
+++ client-admin/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import { Navbar, Form } from "react-bootstrap";
+
+export default function NavBar() {
+  return (
+    <Navbar className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
+      <Navbar.Brand href="#home">
+        <img
+          src={process.env.PUBLIC_URL + "/assets/letterboxd-logo-dark.png"}
+          height="auto"
+          width="300"
+          className="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6"
+          alt="Letterboxd logo"
+        />
+      </Navbar.Brand>
+      <Form className="justify-content-start">
+        <Form.Control
+          type="text"
+          placeholder="Search"
+          className="form-control form-control-dark w-300 rounded-0 border-0"
+          bg="light"
+          aria-label="Search"
+        />
+      </Form>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-center"></Navbar.Collapse>
+    </Navbar>
+  );
+}
