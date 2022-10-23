83856b8dae1a8e7658ddb3d011f605c4d44d6623
yasminnabila <yasmnnabila@gmail.com>
2022-10-23 17:50:00 +0700

    Process deployment

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const router = require("./routers");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
